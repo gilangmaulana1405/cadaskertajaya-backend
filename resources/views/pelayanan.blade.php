05f6ef2c34c104e20cceea3a53275e3369b7db18
Gilang <gilangmaulana541@gmail.com>
2023-09-21 00:47:50 +0700

    edit menu pelayanan

--- resources/views/pelayanan.blade.php
+++ resources/views/pelayanan.blade.php
@@ -18,13 +18,12 @@
                     </div>
                 </div>
             </div>
-            <!-- End Course Item-->
 
             <div class="col-lg-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-toggle="modal" data-target="#ModelSKM">
                 <div class="course-item">
-                    <img src="assets/img/SKTM.png" class="img-fluid" alt="..." />
+                    <img src="assets/img/SKM.png" class="img-fluid" alt="..." />
                     <div class="course-content">
-                        <h3><a data-toggle="modal" data-target="#ModelSKM">SKM (Surat Keterangan Kematian)</a>
+                        <h3><a data-toggle="modal" data-target="#ModelSKM">SKM (Surat Keterangan Meninggal)</a>
                         </h3>
                         <p>Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae
                             dolores dolorem
@@ -32,11 +31,23 @@
                     </div>
                 </div>
             </div>
-            <!-- End Course Item-->
+
+            <div class="col-lg-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-toggle="modal" data-target="#ModelSKTM">
+                <div class="course-item">
+                    <img src="assets/img/SKTM.png" class="img-fluid" alt="..." />
+                    <div class="course-content">
+                        <h3><a data-toggle="modal" data-target="#ModelSKM">SKM (Surat Keterangan Tidak Mampu)</a>
+                        </h3>
+                        <p>Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae
+                            dolores dolorem
+                            tempore.</p>
+                    </div>
+                </div>
+            </div>
 
             <div class="col-lg-3 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0" data-toggle="modal" data-target="#ModelSKdomisili">
                 <div class="course-item">
-                    <img src="assets/img/skdomisili.png" class="img-fluid" alt="..." />
+                    <img src="assets/img/SK_domisili.png" class="img-fluid" alt="..." />
                     <div class="course-content">
                         <h3><a data-toggle="modal" data-target="#ModelSKdomisili">SK Domisili</a></h3>
                         <p>Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae
@@ -45,18 +56,6 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0" data-toggle="modal" data-target="#ModelSKdomisili">
-                <div class="course-item">
-                    <img src="assets/img/skdomisili.png" class="img-fluid" alt="..." />
-                    <div class="course-content">
-                        <h3><a data-toggle="modal" data-target="#ModelSKdomisili">SK Domisili</a></h3>
-                        <p>Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae
-                            dolores dolorem
-                            tempore.</p>
-                    </div>
-                </div>
-            </div>
-            <!-- End Course Item-->
         </div>
     </div>
 </section>
